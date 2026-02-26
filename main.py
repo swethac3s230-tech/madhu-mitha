7b2eae32b46000686c6c1dff41ea46f53275383c
swethac3s230-tech <swethac3s230@gmail.com>
2026-02-26 16:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-
 with st.sidebar:
     data = option_menu(
         menu_title = "my project",
-        option =[
+        options = [
         "home",
         "about",
         "contract",
